61d48ec029dfd0d36536651fd6dab94d2037b94c
Rene8888 <rene.hollander@hotmail.de>
2014-02-18 08:14:15 +0100

    Added a test package with some classes

---
+++ src/hackenbergerhollander/test/CipherTest.java
@@ -0,0 +1,19 @@
+package hackenbergerhollander.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test class for all ciphers
+ * 
+ * @author Hackenberger Christoph
+ */
+public class CipherTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/hackenbergerhollander/test/GuiTest.java
@@ -0,0 +1,19 @@
+package hackenbergerhollander.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test class for all gui tests
+ * 
+ * @author Rene Hollander
+ */
+public class GuiTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
