a265b765908981ba54bf04f1b729c7458f74d1fb
Rene8888 <rene.hollander@hotmail.de>
2014-02-18 09:30:24 +0100

    Added some gui test cases! keyword cipher is still missing!

--- src/hackenbergerhollander/test/GuiTest.java
+++ src/hackenbergerhollander/test/GuiTest.java
@@ -1,6 +1,10 @@
 package hackenbergerhollander.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import hackenbergerhollander.Control;
+import hackenbergerhollander.KeywordCipher;
+import hackenbergerhollander.SubType;
+import hackenbergerhollander.SubstitutionCipher;
 
 import org.junit.Test;
 
@@ -9,11 +13,43 @@ import org.junit.Test;
  * 
  * @author Rene Hollander
  */
+@SuppressWarnings("javadoc")
 public class GuiTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testShift() throws InterruptedException {
+		Control c = new Control();
+		c.view.subType.setSelectedItem(SubType.SHIFT);
+		c.view.sub.setText("2");
+		c.view.message.setText("Hallo Welt!");
+		c.view.encrypt.doClick();
+		assertEquals("jcnnq ygnv!", c.view.message.getText());
+		c.view.decrypt.doClick();
+		assertEquals("hallo welt!", c.view.message.getText());
+	}
+	
+	@Test
+	public void testKeyword() throws InterruptedException {
+		Control c = new Control();
+		c.view.subType.setSelectedItem(SubType.KEY);
+		c.view.sub.setText("kryptos");
+		c.view.message.setText("Hallo Welt!");
+		c.view.encrypt.doClick();
+		assertEquals("jcnnq ygnv!", c.view.message.getText());
+		c.view.decrypt.doClick();
+		assertEquals("hallo welt!", c.view.message.getText());
+	}
+	
+	@Test
+	public void testSubstitution() throws InterruptedException {
+		Control c = new Control();
+		c.view.subType.setSelectedItem(SubType.SUBST);
+		c.view.sub.setText("regänschimtöadbfjüklopquvßwxyz");
+		c.view.message.setText("Hallo Welt!");
+		c.view.encrypt.doClick();
+		assertEquals("hrööb qnöl!", c.view.message.getText());
+		c.view.decrypt.doClick();
+		assertEquals("hallo welt!", c.view.message.getText());
 	}
 
 }
