cea18ae168d91c8bcf74b3378416cc92c2a546c3
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2014-02-18 11:29:34 +0100

    added testcases for cipher classes

--- src/hackenbergerhollander/test/CipherTest.java
+++ src/hackenbergerhollander/test/CipherTest.java
@@ -1,6 +1,11 @@
 package hackenbergerhollander.test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+import hackenbergerhollander.KeywordCipher;
+import hackenbergerhollander.MonoalphabeticCipher;
+import hackenbergerhollander.ShiftCipher;
+import hackenbergerhollander.SubstitutionCipher;
+import hackenbergerhollander.Util;
 
 import org.junit.Test;
 
@@ -13,8 +18,87 @@ import org.junit.Test;
 public class CipherTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMonoAlphabeticConstructor() {
+		assertEquals(Util.getAlphabet(), new MonoalphabeticCipher().getSecretAlphabet());
 	}
 
+	@Test
+	public void testSetSecretAlphabetMonoalphabeticCipher() {
+		MonoalphabeticCipher c = new MonoalphabeticCipher();
+		c.setSecretAlphabet("ßüöäzyxwvutsrqponmlkjihgfedcba".toCharArray());
+		assertArrayEquals("ßüöäzyxwvutsrqponmlkjihgfedcba".toCharArray(), c.getSecretAlphabet());
+	}
+	
+	@Test
+	public void testEncryptMonoalphabeticCipher1() {
+		MonoalphabeticCipher c = new MonoalphabeticCipher();
+		assertEquals("hallo welt!", c.encrypt("Hallo Welt!"));
+	}
+	
+	@Test
+	public void testEncryptMonoalphabeticCipher2() {
+		MonoalphabeticCipher c = new MonoalphabeticCipher();
+		c.setSecretAlphabet("ßüöäzyxwvutsrqponmlkjihgfedcba".toCharArray());
+		assertEquals("wßssp hzsk!", c.encrypt("Hallo Welt!"));
+	}
+	
+	@Test
+	public void testDecryptMonoalphabeticCipher1() {
+		MonoalphabeticCipher c = new MonoalphabeticCipher();
+		assertEquals("hallo welt!", c.decrypt("Hallo Welt!"));
+	}
+	
+	@Test
+	public void testDecryptMonoalphabeticCipher2() {
+		MonoalphabeticCipher c = new MonoalphabeticCipher();
+		c.setSecretAlphabet("ßüöäzyxwvutsrqponmlkjihgfedcba".toCharArray());
+		assertEquals("hallo welt!", c.decrypt("wßssp hzsk!"));
+	}
+	
+	@Test
+	public void testSubstitutionCipherConstructor1() {
+		assertArrayEquals(Util.getAlphabet(), new SubstitutionCipher(Util.getAlphabet()).getSecretAlphabet());
+	}
+	
+	@Test
+	public void testSubstitutionCipherConstructor2() {
+		assertArrayEquals("abcdefghijklmnopqrstuvwxyzäöüß".toCharArray(), new SubstitutionCipher("abcdefghijklmnopqrstuvwxyzäöüß").getSecretAlphabet());
+	}
+	
+	@Test
+	public void testSetSecretAlphabetSubstitutionCipher1() {
+		SubstitutionCipher c = new SubstitutionCipher("abcdefghijklmnopqrstuvwxyzäöüß");
+		c.setSecretAlphabet("ßüöäzyxwvutsrqponmlkjihgfedcba");
+		assertArrayEquals("ßüöäzyxwvutsrqponmlkjihgfedcba".toCharArray(), c.getSecretAlphabet());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetSecretAlphabetSubstitutionCipher2() {
+		new SubstitutionCipher("abcdefghijklmnopqrstuvwxyzäöü");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testSetSecretAlphabetSubstitutionCipher3() {
+		new SubstitutionCipher("abc");
+	}
+	
+	@Test
+	public void testShiftCipherConstructor1() {
+		assertArrayEquals(Util.getAlphabet(), new ShiftCipher(0).getSecretAlphabet());
+	}
+	
+	@Test
+	public void testShiftCipherConstructor2() {
+		assertArrayEquals("ghijklmnopqrstuvwxyzäöüßabcdef".toCharArray(), new ShiftCipher(6).getSecretAlphabet());
+	}
+	
+	@Test
+	public void testKeywordCipherConstructor1() {
+		assertArrayEquals("REGNSCHIMABDFJKLOPQTUVWXYZÄÖÜß".toLowerCase().toCharArray(), new KeywordCipher("Regenschirm").getSecretAlphabet());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testKeywordCipherConstructor2() {
+		new KeywordCipher("Regenschirm!");
+	}
 }
